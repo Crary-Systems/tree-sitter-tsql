1f367ebab16e3c0d6b8fba1c1024d2c7dac6198e
George Takumi Crary <tcrary@gmail.com>
2024-10-03 22:11:07 -0700

    Fix batch and add TODOs
    The convention going forward is anything thats finished will link the
    ANTLR rule and doc above the rule.
    Anything unfinished will be left as a TODO

--- grammar.js
+++ grammar.js
@@ -26,12 +26,14 @@ module.exports = grammar({
   ],
 
   rules: {
+
+    //https://github.com/antlr/grammars-v4/blob/master/sql/tsql/TSqlParser.g4#L35
     tsql_file: $ => choice(
       repeat($.batch)
-      //TODO execute_body_batch go_statmenet* -- https://github.com/antlr/grammars-v4/blob/master/sql/tsql/TSqlParser.g4#L37
+      ,seq($.execute_body_batch, repeat($.go_statement))
     ),
 
-    batch: $ => prec.left(choice(
+    batch: $ => prec.left(1,choice(
       $.go_statement
       ,seq(optional($.execute_body_batch),choice($.go_statement, repeat1($.sql_clauses)), repeat($.go_statement))
       ,//TODO seq($.batch_level_statement, repeat($.go_statement))
@@ -40,29 +42,32 @@ module.exports = grammar({
     //https://learn.microsoft.com/en-us/sql/t-sql/language-elements/sql-server-utilities-statements-go?view=sql-server-ver16
     go_statement: $ => prec(2,seq(token(/GO/i), optional(field("count", $.integer)))),
 
-    //TODOWORKME
+    //https://github.com/antlr/grammars-v4/blob/master/sql/tsql/TSqlParser.g4#L3145
     execute_body_batch: $ => seq(
       $.func_proc_name_server_database_schema, optional(seq($.execute_statement_arg, repeat(seq(token(','), $.execute_statement_arg)))), optional(SEMI)
     ),
 
+    //https://github.com/antlr/grammars-v4/blob/master/sql/tsql/TSqlParser.g4#L5145
     func_proc_name_server_database_schema: $ => choice(
-     //TODO seq(optional(field('server', $.id_)), DOT, optional(field('database', $.id_)), DOT, optional(field('schema', $.id_)), DOT, field('procedure', $.id_))
-      $.func_proc_name_database_schema
+      seq(optional(field('server', $.id_)), DOT, optional(field('database', $.id_)), DOT, optional(field('schema', $.id_)), DOT, field('procedure', $.id_))
+      ,$.func_proc_name_database_schema
     ),
 
+    //https://github.com/antlr/grammars-v4/blob/master/sql/tsql/TSqlParser.g4#L5140
     func_proc_name_database_schema: $ => choice(
       seq(optional(field('database', $.id_)), DOT, optional(field('schema', $.id_)), DOT, field('procedure', $.id_))
       ,$.func_proc_name_schema
     ),
 
-    //https://github.com/antlr/grammars-v4/blob/master/sql/tsql/TSqlParser.g4#L5136-L5138
+    //https://github.com/antlr/grammars-v4/blob/master/sql/tsql/TSqlParser.g4#L5136
     func_proc_name_schema: $ => seq(optional(seq(field('schema',$.id_), DOT)), field('procedure', $.id_)),
 
     execute_statement_arg: $ => choice(
-      //seq($.execute_statement_arg_unnamed, repeat(seq(token(','), $.execute_statement_arg))),   //Unnamed params can continue unnamed
+      //TODO seq($.execute_statement_arg_unnamed, repeat(seq(token(','), $.execute_statement_arg))),   //Unnamed params can continue unnamed
       prec.left(seq($.execute_statement_arg_named, repeat(seq(token(','), $.execute_statement_arg_named)))) //Named can only be continued by unnamed
     ),
 
+    //https://github.com/antlr/grammars-v4/blob/master/sql/tsql/TSqlParser.g4#L3163
     execute_statement_arg_named: $ => seq(
       field('name', LOCAL_ID), token('='), field('value', $.execute_parameter)
     ),
@@ -84,16 +89,21 @@ module.exports = grammar({
 
     sql_clauses: $ => choice(
       seq($.dml_clause, optional(SEMI))
+      //TODO https://github.com/antlr/grammars-v4/blob/master/sql/tsql/TSqlParser.g4#L53-L61
     ),
+
 
     dml_clause: $ => choice(
       $.select_statement_standalone
+      //TODO https://github.com/antlr/grammars-v4/blob/master/sql/tsql/TSqlParser.g4#L63-L70
     ),
 
     select_statement_standalone: $ => seq(
+      //TODO with_expression https://github.com/antlr/grammars-v4/blob/master/sql/tsql/TSqlParser.g4#L2183
       $.select_statement
     ),
 
+    //https://github.com/antlr/grammars-v4/blob/master/sql/tsql/TSqlParser.g4#L2186
     select_statement: $ => prec.left(seq(
       $.query_expression
       ,optional(SEMI)
@@ -101,28 +111,41 @@ module.exports = grammar({
 
     query_expression: $ => seq(
       $.query_specification
+      //TODO union all https://github.com/antlr/grammars-v4/blob/master/sql/tsql/TSqlParser.g4#L3999
     ),
 
     query_specification: $ => seq(
       token(/SELECT/i)
       ,$.select_list
       ,optional(seq(token(/FROM/i), $.table_sources))
+      //TODO https://github.com/antlr/grammars-v4/blob/master/sql/tsql/TSqlParser.g4#L4010-L4023
     ),
 
+    //https://learn.microsoft.com/en-us/sql/t-sql/queries/select-clause-transact-sql?view=sql-server-ver16&redirectedfrom=MSDN
+    //https://github.com/antlr/grammars-v4/blob/master/sql/tsql/TSqlParser.g4#L4119
     select_list: $ => seq($.select_list_elem, repeat(seq(token(','), $.select_list_elem))),
 
-    select_list_elem: $ => choice(ID, SQUARE_BRACKET_ID),
+    //TODO REDO THIS ONE https://github.com/antlr/grammars-v4/blob/master/sql/tsql/TSqlParser.g4#L4143-L4148
+    select_list_elem: $ => choice(
+      ID, SQUARE_BRACKET_ID
+    ),
 
-    table_sources: $ => seq($.table_source),
+    table_sources: $ => choice(
+      $.table_source
+      //TODO https://github.com/antlr/grammars-v4/blob/master/sql/tsql/TSqlParser.g4#L4150-L4153
+    ),
 
+    //TODO https://github.com/antlr/grammars-v4/blob/master/sql/tsql/TSqlParser.g4#L4160C4-L4163
     table_source: $ => seq($.table_source_item),
 
     table_source_item: $ => choice(
       seq($.full_table_name)
+      //TODO https://github.com/antlr/grammars-v4/blob/master/sql/tsql/TSqlParser.g4#L4165-L4183
     ),
 
-    full_table_name: $ => seq(
+    full_table_name: $ => choice(
       $.id_
+      //TODO https://github.com/antlr/grammars-v4/blob/master/sql/tsql/TSqlParser.g4#L5116-L5123
     ),
 
     //TODO https://github.com/antlr/grammars-v4/blob/master/sql/tsql/TSqlParser.g4#L5155-L5160
@@ -132,6 +155,7 @@ module.exports = grammar({
 
     expression: $ => choice(
       $.full_column_name
+      //TODO https://github.com/antlr/grammars-v4/blob/master/sql/tsql/TSqlParser.g4#L3900-L3917
     ),
 
     //
@@ -144,7 +168,7 @@ module.exports = grammar({
       ,$.keyword
     ),
 
-    //https://github.com/antlr/grammars-v4/blob/master/sql/tsql/TSqlParser.g4#L5287
+    //TODO https://github.com/antlr/grammars-v4/blob/master/sql/tsql/TSqlParser.g4#L5287
     keyword: $ => choice(
       token(/GO/i)
     ),
