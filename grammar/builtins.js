041444bb580b5c1bf31b30838be3a1c5afa7b86e
George Takumi Crary <tcrary@gmail.com>
2024-10-13 21:45:21 -0700

    Add ASSEMBLYPROPERTY, COL_LENGTH, COL_NAME

--- grammar/builtins.js
+++ grammar/builtins.js
@@ -27,9 +27,31 @@ module.exports = {
         ,field('resource_name', $.expression)
         ,field('lock_mode', $.expression)
         ,field('lock_owner', $.expression)))
+
+      // https://docs.microsoft.com/en-us/sql/t-sql/functions/assemblyproperty-transact-sql?view=sql-server-ver16
+      // ASSEMBLYPROPERTY
+      ,seq($.assemblyproperty_, parensComma(
+        field('assembly_name', $.expression)
+        ,field('property_name', $.expression)))
+
+      // https://learn.microsoft.com/en-us/sql/t-sql/functions/col-length-transact-sql?view=sql-server-ver16
+      // COL_LENGTH
+      ,seq($.col_length_, parensComma(
+        field('table', $.expression)
+        ,field('column', $.expression)))
+
+      // https://learn.microsoft.com/en-us/sql/t-sql/functions/col-name-transact-sql?view=sql-server-ver16
+      // COL_NAME
+      ,seq($.col_name_, parensComma(
+        field('table_id', $.expression)
+        ,field('column_id', $.expression)))
+
     ),
 
     app_name_: $ => token(/APP_NAME/i),
     applock_mode_: $ => token(/APPLOCK_MODE/i),
     applock_test_: $ => token(/APPLOCK_TEST/i),
+    assemblyproperty_: $ => token(/ASSEMBLYPROPERTY/i),
+    col_length_: $ => token(/COL_LENGTH/i),
+    col_name_: $ => token(/COL_NAME/i),
 };
