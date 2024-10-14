1b9fbe270939893233d697bfd26b1b07122d42f5
George Takumi Crary <tcrary@gmail.com>
2024-10-13 19:54:53 -0700

    Add APPLOCK_TEST and parensComma helper

--- grammar/builtins.js
+++ grammar/builtins.js
@@ -1,5 +1,10 @@
 const COMMA = token(',');
 const parens = (...rule) => seq('(', ...rule, ')');
+const parensComma = (...args) => parens(comma_separated(...args));
+
+function comma_separated(...rules) {
+  return seq(...rules.flatMap((rule, index) => index === 0 ? [rule] : [',', rule]));
+}
 
 module.exports = {
     //https://github.com/antlr/grammars-v4/blob/master/sql/tsql/TSqlParser.g4#L4338
@@ -10,12 +15,21 @@ module.exports = {
 
       // https://docs.microsoft.com/en-us/sql/t-sql/functions/applock-mode-transact-sql?view=sql-server-ver16
       // APPLOCK_MODE
-      ,seq($.applock_mode_, parens(
-        field('database_principal', $.expression), COMMA
-        ,field('resource_name', $.expression), COMMA
+      ,seq($.applock_mode_, parensComma(
+        field('database_principal', $.expression)
+        ,field('resource_name', $.expression)
+        ,field('lock_owner', $.expression)))
+
+      // https://docs.microsoft.com/en-us/sql/t-sql/functions/applock-test-transact-sql?view=sql-server-ver16
+      // APPLOCK_TEST
+      ,seq($.applock_test_, parensComma(
+        field('database_principal', $.expression)
+        ,field('resource_name', $.expression)
+        ,field('lock_mode', $.expression)
         ,field('lock_owner', $.expression)))
     ),
 
     app_name_: $ => token(/APP_NAME/i),
     applock_mode_: $ => token(/APPLOCK_MODE/i),
+    applock_test_: $ => token(/APPLOCK_TEST/i),
 };
